74ee7b6d37f2902a4a8e03f9c7ec3375a5a18a3e
Nate Meyers <natedaswas@gmail.com>
2017-04-20 13:44:17 -0600

    first commit containing nothing but little sqlite hello world type file

---
+++ gosql.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	db, err := sql.Open("mysql", "user:password@tcp(127.0.0.1:3306)/hello")
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := db.Ping(); err != nil {
+		panic(err)
+	}
+	defer db.Close()
+}
+
